ef41b50c6e55dac9fb1d90d1e7bdd7e0016a6441
richamukasa <92112994+richamukasa@users.noreply.github.com>
2022-04-09 20:19:27 +0200

    L03_Konzept

--- L03/script.js
+++ L03/script.js
@@ -6,9 +6,12 @@ function handleLoad(_event) {
     let wrapper = document.getElementById("wrapper");
     let valueArray = [];
     //valueArray = schuffle (valueArray)
-    let y = 0;
+    let y = 1;
     let divCounter = 0;
     let cardDiv;
+    let timeStart;
+    let clickedCard1;
+    let clickedCard2;
     //Anzahl der Paare abfragen
     let cardQuestion = prompt("wie viele Kartenpaare", "5-25");
     let cardQuestionNumber = parseFloat(cardQuestion);
@@ -21,10 +24,11 @@ function handleLoad(_event) {
         createCards();
     }
     function createValues() {
-        for (let i = 1; i < cardQuestionNumber; i++) {
-            valueArray.push(i);
-            valueArray.push(i);
+        for (let i = 0; i < cardQuestionNumber; i++) {
+            valueArray.push(i + 1);
+            valueArray.push(i + 1);
         }
+        //console.log(valueArray);
     }
     function createCards() {
         for (let x = 0; x < cardQuestionNumber; x++) {
@@ -36,24 +40,41 @@ function handleLoad(_event) {
             cardDiv.addEventListener("click", cardClicked);
             //cardDiv.style = PointerEvent;
         }
-        let timeStart = Date.now();
-        console.log(timeStart);
+        timeStart = Date.now();
+        //console.log(timeStart);
     }
     function cardClicked(_event) {
         let clickedCard = _event.target;
-        if (y < 3) { // && cardDiv.style.opacity = "0"
+        if (y == 1 && clickedCard.style.opacity == "0") {
             clickedCard.style.opacity = "100";
             y++;
-            //clickedCard = clickedCard + y; // keine Ahnung was genau hier passieren soll
+            clickedCard1 = clickedCard;
         }
-        if (y === 2) {
-            window.setTimeout(() => {
-                valueCheck();
-            }, 2000);
+        if (y == 2 && clickedCard.style.opacity == "0") {
+            clickedCard.style.opacity = "100";
+            y++;
+            clickedCard2 = clickedCard;
         }
+        window.setTimeout(() => {
+            valueCheck();
+        }, 2000);
     }
     function valueCheck() {
         console.log("valueCheck");
+        if (clickedCard1 == clickedCard2) {
+            if (cardDiv.style.opacity == "100") {
+                remove(cardDiv);
+                divCounter--;
+                if (divCounter == 0) {
+                    let timeEnd = Date.now();
+                    let timePassed = timeEnd - timeStart;
+                    alert("Spiel fertig! Zeit:" + timePassed);
+                }
+            }
+        }
+        else {
+            cardDiv.style.opacity = "0";
+        }
     }
 }
 //# sourceMappingURL=script.js.map
--- L03/script.ts
+++ L03/script.ts
@@ -1,64 +1,80 @@
-window.addEventListener("load", handleLoad);
-function handleLoad(_event: Event): void {
-    console.log("load");
+//Grundgerüst anlegen
+let wrapper: HTMLDivElement = <HTMLDivElement>document.getElementById("wrapper");
+let valueArray: number [] = [];
+//valueArray = schuffle (valueArray)
+let y: number = 1;
+let divCounter: number = 0;
+let cardDiv: HTMLDivElement;
+let timeStart: number;
+let clickedCard1: HTMLDivElement;
+let clickedCard2: HTMLDivElement;
 
-    //Grundgerüst anlegen
-    let wrapper: HTMLDivElement = <HTMLDivElement>document.getElementById("wrapper");
-    let valueArray: number [] = [];
-    //valueArray = schuffle (valueArray)
-    let y: number = 0;
-    let divCounter: number = 0;
-    let cardDiv: HTMLDivElement;
+//Anzahl der Paare abfragen
+let cardQuestion: string = <string>prompt ("wie viele Kartenpaare", "5-25");
+let cardQuestionNumber: number = parseFloat(cardQuestion);
+//console.log(cardQuestionNumber);
+if (cardQuestionNumber < 5 || cardQuestionNumber > 25) {
+    window.location.reload();
+}
+else {
+    createValues();
+    createCards();
+}
 
-    //Anzahl der Paare abfragen
-    let cardQuestion: string = <string>prompt ("wie viele Kartenpaare", "5-25");
-    let cardQuestionNumber: number = parseFloat(cardQuestion);
-    //console.log(cardQuestionNumber);
-    if (cardQuestionNumber < 5 || cardQuestionNumber > 25) {
-        window.location.reload();
+function createValues (): void {
+    for (let i: number  = 0; i < cardQuestionNumber; i++ ) {
+        valueArray.push(i + 1);
+        valueArray.push(i + 1);
+    }
+    //console.log(valueArray);
+}
+
+function createCards (): void {
+    for (let x: number = 0; x < cardQuestionNumber; x++) {
+        cardDiv = document.createElement ("div");
+        divCounter ++;
+        wrapper.appendChild(cardDiv);
+        cardDiv.innerHTML = valueArray.splice (1, 0) + "";
+        cardDiv.style.opacity = "0";
+        cardDiv.addEventListener ("click", cardClicked);
+        //cardDiv.style = PointerEvent;
+    }
+    timeStart = Date.now ();
+    //console.log(timeStart);
+}
+
+function cardClicked (_event: MouseEvent): void {
+    let clickedCard: HTMLDivElement = <HTMLDivElement> _event.target;
+    if (y == 1 && clickedCard.style.opacity == "0") {
+        clickedCard.style.opacity = "100";
+        y++;
+        clickedCard1 = clickedCard;
+    }
+    if (y == 2 && clickedCard.style.opacity == "0") {
+        clickedCard.style.opacity = "100";
+        y++;
+        clickedCard2 = clickedCard;
+    }
+    window.setTimeout(() => {
+            valueCheck ();
+        },            2000);
+    }
+
+function valueCheck (): void {
+    console.log("valueCheck");
+    if (clickedCard1 == clickedCard2) {
+        if (cardDiv.style.opacity == "100") {
+            remove (cardDiv);
+            divCounter --;
+            if ( divCounter == 0) {
+                let timeEnd: number = Date.now ();
+                let timePassed: number = timeEnd - timeStart;
+                alert ("Spiel fertig! Zeit:" + timePassed);
+            }
+        }
+
     }
     else {
-        createValues();
-        createCards();
+        cardDiv.style.opacity = "0";
     }
-    
-    function createValues (): void {
-        for (let i: number  = 1; i < cardQuestionNumber; i++ ) {
-            valueArray.push(i);
-            valueArray.push(i);
-        }
-    }
-
-    function createCards (): void {
-        for (let x: number = 0; x < cardQuestionNumber; x++) {
-            cardDiv = document.createElement ("div");
-            divCounter ++;
-            wrapper.appendChild(cardDiv);
-            cardDiv.innerHTML = valueArray.splice (1, 0) + "";
-            cardDiv.style.opacity = "0";
-            cardDiv.addEventListener ("click", cardClicked);
-            //cardDiv.style = PointerEvent;
-        }
-        let timeStart: number = Date.now ();
-        console.log(timeStart);
-    }
-
-    function cardClicked (_event: MouseEvent): void {
-        let clickedCard: HTMLDivElement = <HTMLDivElement> _event.target;
-        if (y < 3) { // && cardDiv.style.opacity = "0"
-            clickedCard.style.opacity = "100";
-            y++;
-            //clickedCard = clickedCard + y; // keine Ahnung was genau hier passieren soll
-        }
-        if (y === 2) {
-            window.setTimeout(() => {
-                valueCheck ();
-            },                2000);
-        }
-    }
-
-    function valueCheck (): void {
-        console.log("valueCheck");
-    }
-
 }
